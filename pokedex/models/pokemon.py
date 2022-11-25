9bca3156ca86911e5ba6ba90a56d2d22556d65ce
CarlosPozoC <cpozocatalan@gmail.com>
2022-11-25 18:11:19 +0100

    Relaccion many2many funcional

--- pokedex/models/pokemon.py
+++ pokedex/models/pokemon.py
@@ -4,7 +4,8 @@ class pokemon(models.Model):
     _name='pokedex.pokemon'
     _description='Permite dar una descripcion breve de un pokemon'
     _inherit='pokedex.base'
-
+    
+    
     name = fields.Char(string='Nombre del pokemon', required=True)
     description = fields.Text(string='Descripcion del pokemon')
-
+    tipo_ids=fields.Many2many('pokedex.tipo',string="Tipo")
